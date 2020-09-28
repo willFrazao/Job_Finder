3391c15eabdd570a8c5be759f4e4576b680611ce
willFrazao <william.frazao91@gmail.com>
2020-09-28 19:53:44 -0300

    configurando o banco de dados

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const db = require('./db/connection');
 
 const PORT = 3000;
 
@@ -7,6 +8,17 @@ app.listen(PORT, function() {
     console.log(`o express está rodando na porta ${PORT}`);
 });
 
+// db connection
+db
+    .authenticate()
+    .then(() => {
+        console.log('conectou ao banco de dados com sucesso');
+    })
+    .catch(err => {
+        console.log('ocorreu um erro ao conectar', err);
+    });
+
+// routes
 app.get('/', (req, res) => {
     res.send('Está Funcionando');
 });
