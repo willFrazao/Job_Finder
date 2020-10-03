7ff2869997b3db6a4dea6ed31aaf3c813f7e8e59
willFrazao <william.frazao91@gmail.com>
2020-10-03 11:32:32 -0300

    criando rota para adicionar registros

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
-const express = require('express');
-const app = express();
-const db = require('./db/connection');
+const express    = require('express');
+const app        = express();
+const db         = require('./db/connection');
+const bodyParser = require('body-parser');
 
 const PORT = 3000;
 
 app.listen(PORT, function() {
     console.log(`o express está rodando na porta ${PORT}`);
 });
+
+// body parser
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // db connection
 db
@@ -22,3 +26,6 @@ db
 app.get('/', (req, res) => {
     res.send('Está Funcionando');
 });
+
+// jobs routes
+app.use('/jobs', require('./routes/jobs'));
---
+++ routes/jobs.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const router  = express.Router();
+const Job     = require('../models/Job');
+
+router.get('/test', (req, res) => {
+    res.send('deu certo');
+});
+
+// add job via post
+router.post('/add', (req, res) => {
+
+    let {title, salary, company, description, email, new_job} = req.body;
+
+    // insert
+    Job.create({
+        title,
+        salary,
+        company,
+        description,
+        email,
+        new_job
+    })
+    .then(() => res.redirect('/'))
+    .catch(err => console.log(err));
+
+});
+
+module.exports = router
