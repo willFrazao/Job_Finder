e2037c7d545619f9d2e52a807581c436c81eaf3c
willFrazao <william.frazao91@gmail.com>
2020-10-12 00:26:34 -0300

    fazendo a logica da busca

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
-const express    = require('express');
-const exphbs     = require('express-handlebars')
-const app        = express();
-const path       = require('path');
-const db         = require('./db/connection');
-const bodyParser = require('body-parser');
+const express       = require('express');
+const exphbs        = require('express-handlebars')
+const app           = express();
+const path          = require('path');
+const db            = require('./db/connection');
+const bodyParser    = require('body-parser');
+const job           = require('./models/Job');
+const Job           = require('./models/Job');
+const sequelize     = require('sequelize');
+const Op            = sequelize.Op;
 
 const PORT = 3000;
 
-app.listen(PORT, function() {
+app.listen(PORT, function () {
     console.log(`o express está rodando na porta ${PORT}`);
 });
 
@@ -17,7 +21,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // handle bars
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'handlebars');
-app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 
 // static folder
 app.use(express.static(path.join(__dirname, 'public')));
@@ -34,7 +38,36 @@ db
 
 // routes
 app.get('/', (req, res) => {
-    res.render('index');
+
+    let search = req.query.job;
+    let query  = '%'+search+'%'; // PH -> PHP, Word -> Wordpress funcionamento da busca.
+
+    if (!search) {
+        Job.findAll({
+            order: [
+                ['createdAt', 'DESC']
+            ]
+        })
+            .then(jobs => {
+                res.render('index', {
+                    jobs
+                });
+            })
+            .catch(err => console.log(err));
+    } else {
+        Job.findAll({
+            where: {title: {[Op.like]: query}},
+            order: [
+                ['createdAt', 'DESC']
+            ]
+        })
+            .then(jobs => {
+                res.render('index', {
+                    jobs, search
+                });
+            });
+    }
+
 });
 
 // jobs routes
